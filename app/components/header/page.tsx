9e75f2493e604a7c47ff0894cacbd70b25a244d2
LuanderV <luander.vargas@gmail.com>
2025-02-19 23:24:39 -0300

    Header, imagens e biblioteca swiper
    Imagens, animações e lógica básica implementadas ao componente Header.
    
    Biblioteca Swiper implementada ao projeto para uso de carrosseis dinâmicos e responsivos.

--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -1,30 +1,59 @@
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { FiSearch, FiHeart, FiUser, FiShoppingBag } from "react-icons/fi";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export default function Header() {
   return (
-    <header className="bg-gray-50">
-      <div className="flex justify-center py-4">
-        <Image 
-          src="/img/logo.png" 
-          alt="Logo da Loja" 
-          width={300} 
-          height={80} 
-        />
-      </div>
+    <div>
+      <header className="absolute top-0 left-0 w-full z-50">
+        <div className="flex justify-end items-center px-8 py-4 gap-8 mx-28">
+          <div className="relative min-w-80">
+            <input
+              type="text"
+              placeholder="Buscar Produtos..."
+              className="w-full pl-4 pr-4 py-2 rounded-full border border-gray-300"
+            />
+            <FiSearch className="absolute right-4 top-2.5 text-gray-600 text-xl" />
+          </div>
 
-      <nav>
-        <ul className="flex justify-center space-x-8 py-2 text-gray-900 font-medium">
-          <li><Link href="#" className="hover:text-gray-600">Lançamentos</Link></li>
-          <li><Link href="#" className="hover:text-gray-600">Sutiã</Link></li>
-          <li><Link href="#" className="hover:text-gray-600">Calcinha</Link></li>
-          <li><Link href="#" className="hover:text-gray-600">Body</Link></li>
-          <li><Link href="#" className="hover:text-gray-600">Moda</Link></li>
-          <li><Link href="#" className="hover:text-gray-600">Pijamas</Link></li>
-          <li><Link href="#" className="hover:text-gray-600">Outlet</Link></li>
-          <li><Link href="#" className="hover:text-gray-600">Chá de Lingerie</Link></li>
-        </ul>
-      </nav>
-    </header>
+          <div className="flex space-x-6 text-white text-2xl">
+            <FiHeart className="hover:text-pink-600 cursor-pointer transition" />
+            <FiUser className="hover:text-pink-600 cursor-pointer transition" />
+            <FiShoppingBag className="hover:text-pink-600 cursor-pointer transition" />
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <Image src="/img/logo.png" alt="Logo da Loja" width={300} height={60} />
+        </div>
+
+        <nav>
+          <div className="flex justify-center space-x-6 py-3 text-white font-medium">
+            {["Lançamentos", "Sutiã", "Calcinha", "Body", "Moda", "Pijamas", "Outlet", "Chá de Lingerie"].map((item) => (
+                <ul key={item} className="hover:border-b-2 hover:border-pink-600 hover:rounded pb-1">
+                    <li>
+                        <Link href="#" className="hover:text-gray-300 transition-colors">{item}</Link>
+                    </li>
+                </ul>
+            ))}
+          </div>
+        </nav>
+      </header>
+
+      <section className="relative w-full h-[900px]">
+        <Swiper loop autoplay={{ delay: 100 }} className="w-full h-full">
+          <SwiperSlide>
+            <Image src="/img/banner-header/banner1.jpg" alt="Banner 1" layout="fill" objectFit="cover" priority />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/img/banner-header/banner2.jpg" alt="Banner 2" layout="fill" objectFit="cover" priority />
+          </SwiperSlide>
+        </Swiper>
+      </section> 
+    </div>
   );
 }
