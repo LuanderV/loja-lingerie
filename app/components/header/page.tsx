1ef8708b66931615124e67be0ad051a71f68b33f
LuanderV <luander.vargas@gmail.com>
2025-02-20 20:11:27 -0300

    att

--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -2,35 +2,18 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { FiSearch, FiHeart, FiUser, FiShoppingBag } from "react-icons/fi";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import Search from "./search";
 
 export default function Header() {
   return (
     <div>
       <header className="absolute top-0 left-0 w-full z-50">
-        <div className="flex justify-end items-center px-8 py-4 gap-8 mx-28">
-          <div className="relative min-w-80">
-            <input
-              type="text"
-              placeholder="Buscar Produtos..."
-              className="w-full pl-4 pr-4 py-2 rounded-full border border-gray-300"
-            />
-            <FiSearch className="absolute right-4 top-2.5 text-gray-600 text-xl" />
-          </div>
-
-          <div className="flex space-x-6 text-white text-2xl">
-            <FiHeart className="hover:text-pink-600 cursor-pointer transition" />
-            <FiUser className="hover:text-pink-600 cursor-pointer transition" />
-            <FiShoppingBag className="hover:text-pink-600 cursor-pointer transition" />
-          </div>
-        </div>
-
+        <Search />
         <div className="flex justify-center">
           <Image src="/img/logo.png" alt="Logo da Loja" width={300} height={60} />
         </div>
-
         <nav>
           <div className="flex justify-center space-x-6 py-3 text-white font-medium">
             {["Lançamentos", "Sutiã", "Calcinha", "Body", "Moda", "Pijamas", "Outlet", "Chá de Lingerie"].map((item) => (
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Header from "./components/header/page";
 
 export default async function Home() {
   return (
-    <>
+    <div>
       <Header />
-    </>
+    </div>
   );
 }
