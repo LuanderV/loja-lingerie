af5683b2e408dfe2e4a63dd3c0250c726a8ce41a
Lithierry Silva <lithy.10@hotmail.com>
2025-02-21 15:59:28 +0000

    feat: Modo animação no icon, e centralização do logo

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,17 +7,17 @@ import { MdOutlineScreenSearchDesktop } from "react-icons/md";
 export default function NotFound() {
   return (
     <Fragment>
-      <header>
+      <header className="flex justify-center p-6">
         <Image src="/img/logo.png" alt="Logo" width={300} height={60}/>
       </header>
-      <div className="flex flex-col items-center justify-center mt-40">
-        <MdOutlineScreenSearchDesktop className="text-8xl" />
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        <MdOutlineScreenSearchDesktop className="text-8xl text-pink-600 animate-bounce" />
         <h1 className="text-6xl font-bold p-10 mb-6">
           404 - Página Não Encontrada
         </h1>
         <Link
           href="/"
-          className="text-black flex items-center justify-center gap-3 font-bold hover:text-gray-600 hover:border-b-2 hover:border-pink-600 hover:rounded pb-1 mt-5 pl-3 pr-3"
+          className="text-black flex items-center justify-center gap-3 font-bold hover:text-gray-800 hover:border-b-2 hover:border-pink-600 hover:rounded pb-1 mt-5 pl-3 pr-3 transition-all"
         >
           <FaArrowRight /> Voltar para o Inicio
         </Link>
